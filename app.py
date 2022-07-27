51e255433f9ee637729205d951eaf2a63216ce50
shaimaa188 <103372088+shaimaa188@users.noreply.github.com>
2022-07-27 12:15:17 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import pickle
+import pandas as pd
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+# create flask app
+app = Flask(__name__)
+
+# load the pickle model
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+# def predict():
+#     float_features = [float(x) for x in request.form.values()]
+#     features = [np.array(float_features)]
+#     prediction = model.predict(features)
+#
+#     return render_template("index.html", prediction_text = "The Species of the flower is {}".format(prediction))
+
+def predict():
+    json_ = request.json
+    query_df = pd.DataFrame(json_)
+    prediction = model.predict(query_df)
+    return jsonify({"Predection": list(prediction)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
